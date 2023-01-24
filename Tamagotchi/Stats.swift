4697affe56e6ef2073b4d7d1ef878edf61429286
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-01-24 10:22:07 +0000

    Testing commit

---
+++ Tamagotchi/Stats.swift
@@ -0,0 +1,18 @@
+//
+//  Stats.swift
+//  Tamagotchi
+//
+//  Created by Wreford, Edward (PGW) on 24/01/2023.
+//
+
+import Foundation
+
+struct Stats {
+    
+    let name: String
+    var age: Int
+    var weight: Int
+    var hunger: Int
+    var healthMeter: Int
+    
+}
