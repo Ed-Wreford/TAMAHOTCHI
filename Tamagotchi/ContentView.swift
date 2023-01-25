661592c69856c56cb1e36cb332c1d6096cdc7901
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-01-25 09:42:23 +0000

    Slight odd button error (too many?)

--- Tamagotchi/ContentView.swift
+++ Tamagotchi/ContentView.swift
@@ -16,34 +16,119 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("\(remainingTime)")
-                .onReceive(timer) { _ in
-                    if self.remainingTime > 0 {
-                        self.remainingTime -= 1
+            Form {
+                Button("Reset", action: {
+                    reset()
+                })
+                Text("Remaining time till hunger reset: \(remainingTime)")
+                    .onReceive(timer) { _ in
+                        if self.remainingTime > 0 {
+                            self.remainingTime -= 1
+                        }
+                        if remainingTime == 0 {
+                            if basicStats.stats[0].healthMeter != 0 {
+                                basicStats.stats[0].hunger = 10
+                                basicStats.stats[0].happiness = "Depressed"
+                                remainingTime = 30
+                            }
+                        }
                     }
-                    if remainingTime == 0 {
-                        basicStats.stats[0].hunger = 10
-                        basicStats.stats[0].happiness = "Depressed"
-                        remainingTime = 30
-                    }
-                }
-                .foregroundColor(.red)
-            
+                    .foregroundColor(.red)
+                
             Text("\(basicStats.stats[0].displayStats())")
             Button("Feed Henry", action: {
                 feedTamagotchi()
             })
+            Button("Feed Henry snack", action: {
+                snackTamagotchi()
+            })
+            Button("Play a game", action: {
+                playGame()
+            })
+            Button("Grow up", action: {
+                growUp()
+            })
+            Button("Release fluids", action: {
+                releaseFluids()
+            })
+            Button("Sick", action: {
+                getSick()
+            })
+            Button("Heal", action: {
+                heal()
+            })
+            Button("Suicide", action: {
+                suicide()
+            })
+            
+            }
+        }
+        
+    }
+    func feedTamagotchi() {
+        if basicStats.stats[0].hunger >= 5 {
+            basicStats.stats[0].hunger -= 5
+            basicStats.stats[0].happiness = "Sad"
+        }
+        
+    }
+    func snackTamagotchi() {
+        if basicStats.stats[0].hunger > 0 {
+            basicStats.stats[0].hunger -= 1
+            basicStats.stats[0].happiness = "I have been snacked"
+        }
+    }
+    func playGame() {
+        basicStats.stats[0].happiness = "Like the end of your birthday"
+    }
+    
+    func growUp() {
+        basicStats.stats[0].age += 1
+        if basicStats.stats[0].age >= 100 {
+            death()
         }
     }
     
-    func feedTamagotchi() {
+    func releaseFluids() {
+        if basicStats.stats[0].weight >= 10 {
+            basicStats.stats[0].weight -= 10
+            death()
+        }
         
-        basicStats.stats[0].hunger -= 5
-        basicStats.stats[0].happiness = "Sad"
-        
+    }
+    func getSick() {
+        if basicStats.stats[0].healthMeter > 0 {
+            basicStats.stats[0].healthMeter -= 1
+        }
+    }
+    
+    func heal() {
+        if basicStats.stats[0].healthMeter < 5 {
+            basicStats.stats[0].healthMeter += 1
+        }
+    }
+    func suicide() {
+        basicStats.stats[0].healthMeter = 0
+        basicStats.stats[0].happiness = "Finally happy"
+    }
+    
+    func death() {
+        if basicStats.stats[0].age > 100 {
+            basicStats.stats[0].healthMeter = 0
+            basicStats.stats[0].happiness = "Died happily"
+        }
+        if basicStats.stats[0].weight == 0 {
+            basicStats.stats[0].healthMeter = 0
+            basicStats.stats[0].happiness = "Died happily"
+        }
+    }
+    
+    func reset() {
         
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
